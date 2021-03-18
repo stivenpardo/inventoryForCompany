c97843902f34d9996fac1b49b74b741049a7e472
Michael Stivens Pardo Burbano <programador2@sypsolutions.com>
2021-03-18 10:17:14 -0500

    implementation process  of Unit Test for Insert in Repository Base

---
+++ InventoryFull.Test.Core/5.Infrastucture/5.1 Data/Base/RepositoryBaseTest.cs
@@ -0,0 +1,34 @@
+﻿using InventoryFull.Domain.Core.Inventory.Category;
+using InvetoryFull.Infrastructure.Data.Persistence.Core.Base;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Categories;
+
+namespace InventoryFull.Test.Core._5.Infrastucture._5._1_Data.Base
+{
+    public class RepositoryBaseTest
+    {
+        [Fact]
+        [UnitTest]
+        public async Task test_insert_call_Add_async_in_UnitOfWork() 
+        {
+            var entityCategory = new CategoryEntity
+            {
+                CategoryId = Guid.NewGuid().ToString(),
+                CategoryName = "Pollo",     
+            };
+
+            var unitOfWorkMock = new Mock<IContextDb>();
+
+            unitOfWorkMock.Setup(m => m.Commit()).Returns(() => 1);
+            //TODO: Michael, finish of unittest
+            unitOfWorkMock.Setup( m => m.Set<CategoryEntity>()).Returns(() => 
+            {
+                var context = new ContextDb();
+            });
+
+        }
+    }
+}
