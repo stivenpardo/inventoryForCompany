ab1a946ce855ff03605abda53d5ba048e21c6831
Michael Stivens Pardo Burbano <programador2@sypsolutions.com>
2021-03-20 13:20:04 -0500

    pending for fix test in repositorybase

--- InventoryFull.Test.Core/5.Infrastucture/5.1 Data/Base/RepositoryBaseTest.cs
+++ InventoryFull.Test.Core/5.Infrastucture/5.1 Data/Base/RepositoryBaseTest.cs
@@ -5,6 +5,7 @@ using InvetoryFull.Infrastructure.Data.Persistence.Core.Base.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Categories;
@@ -30,9 +31,9 @@ namespace InventoryFull.Test.Core._5.Infrastucture._5._1_Data.Base
             unitOfWorkMock.Setup( m => m.Set<CategoryEntity>()).Returns(() => 
             {
                 var context = new ContextDb();
-                context.Categories.Add(entityCategory);
+                context.Category.Add(entityCategory);
                 context.SaveChanges();
-                return context.Categories;
+                return context.Category;
             });
             var services = new ServiceCollection();
             services.AddTransient(_ => unitOfWorkMock.Object);
@@ -47,49 +48,49 @@ namespace InventoryFull.Test.Core._5.Infrastucture._5._1_Data.Base
         }
         [Fact]
         [UnitTest]
-        public void test_Update_call_Add_async_in_UnitOfWork_Successfull() 
+        public async Task test_Update_call_Add_async_in_UnitOfWork_Successfull() 
         {
-            //TODO Michael, solucional la excepción que se esta generando y entender porque sucede.
+            //TODO Michael, solucionar la excepción que se esta generando y entender porque sucede.
             var entityCategory = new CategoryEntity
             {
                 CategoryId = Guid.NewGuid().ToString(),
-                CategoryName = "Pollo",     
+                CategoryName = "Pollo",         
             };
 
-            var repositotyBaseMock = new Mock<IRepositoryBase<CategoryEntity>>();
+            var repositoryBaseMock = new Mock<IRepositoryBase<CategoryEntity>>();
             //var unitOfWorkMock = new Mock<IContextDb>();
 
-            repositotyBaseMock.Setup(m => m.UnitOfWork.Commit()).Returns(() => 1);
+            repositoryBaseMock.Setup(m => m.UnitOfWork.Commit()).Returns(() => 1);
 
-            repositotyBaseMock.Setup( m => m.UnitOfWork.Set<CategoryEntity>()).Returns(() => 
+            repositoryBaseMock.Setup( m => m.UnitOfWork.Set<CategoryEntity>()).Returns(() => 
             {
                 var context = new ContextDb();
-                context.Categories.Update(entityCategory);
+                context.Category.Update(entityCategory);
                 context.SaveChanges();
-                return context.Categories;
+                return context.Category;
             });
-            repositotyBaseMock.Setup(m => m.Update(entityCategory)).Returns(() =>
+            repositoryBaseMock.Setup(m => m.Update(entityCategory)).Returns(() =>
             {
                 return true;
             });
 
             var services = new ServiceCollection();
-            services.AddTransient(_ => repositotyBaseMock.Object);
+            services.AddTransient(_ => repositoryBaseMock.Object);
             services.ConfigureBaseRepository(new DbSettings());
             var provider = services.BuildServiceProvider();
             var categoryRepository = provider.GetRequiredService<ICategoryRepository>();
 
-            var reponse = categoryRepository.Update(new CategoryEntity { CategoryId="1231231", CategoryName="Ninguni"});
+            var reponse = categoryRepository.Update(entityCategory);
 
-            repositotyBaseMock.Verify( x => x.UnitOfWork.Commit(), Times.Once);
-            repositotyBaseMock.Verify( x => x.UnitOfWork.Set<CategoryEntity>(), Times.Once);
+            repositoryBaseMock.Verify( x => x.UnitOfWork.Commit(), Times.Once);
+            repositoryBaseMock.Verify( x => x.UnitOfWork.Set<CategoryEntity>(), Times.Once);
         }
 
         [Fact]
         [UnitTest]
         public void test_Delete_call_Add_async_in_UnitOfWork_Successfull() 
         {
-            //TODO Michael, Hacer que el test pase porque esta pasando por el catch
+            //TODO Michael, solucionar la excepción que se esta generando y entender porque sucede.
             var entityCategory = new CategoryEntity
             {
                 CategoryId = Guid.NewGuid().ToString(),
@@ -103,9 +104,9 @@ namespace InventoryFull.Test.Core._5.Infrastucture._5._1_Data.Base
             unitOfWorkMock.Setup( m => m.Set<CategoryEntity>()).Returns(() => 
             {
                 var context = new ContextDb();
-                context.Categories.Remove(entityCategory);
+                context.Category.Remove(entityCategory);
                 context.SaveChanges();
-                return context.Categories;
+                return context.Category;
             });
 
             var services = new ServiceCollection();
@@ -119,34 +120,5 @@ namespace InventoryFull.Test.Core._5.Infrastucture._5._1_Data.Base
             unitOfWorkMock.Verify( x => x.Commit(), Times.Once);
             unitOfWorkMock.Verify( x => x.Set<CategoryEntity>(), Times.Once);
         }
-        
-        //[Fact]
-        //[UnitTest]
-        //public async Task When_insert_call_add_async_in_UnitOfWorkand_and_the_entity_is_empty_throw_exception()  
-        //{
-        //    var entityCategory = new CategoryEntity();
-
-        //    var unitOfWorkMock = new Mock<IContextDb>();
-
-        //    unitOfWorkMock.Setup(m => m.Commit()).Returns(() => 1);
-      
-        //    unitOfWorkMock.Setup( m => m.Set<CategoryEntity>()).Returns(() => 
-        //    {
-        //        var context = new ContextDb();
-        //        context.Categories.Add(entityCategory);
-        //        context.SaveChanges();
-        //        return context.Categories;
-        //    });
-        //    var services = new ServiceCollection();
-        //    services.AddTransient(_ => unitOfWorkMock.Object);
-        //    services.ConfigureBaseRepository(new DbSettings());
-        //    var provider = services.BuildServiceProvider();
-        //    var categoryRepository = provider.GetRequiredService<ICategoryRepository>();
-
-        //    var reponse = await categoryRepository.Insert(entityCategory);
-
-        //    unitOfWorkMock.Verify( x => x.Commit(), Times.Once);
-        //    unitOfWorkMock.Verify( x => x.Set<CategoryEntity>(), Times.Once);
-        //}
     }
 }
